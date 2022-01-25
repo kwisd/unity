eff866a42e6a166b99df2d341ffb676277c26f1a
kwisd <63222083+kwisd@users.noreply.github.com>
2022-01-25 20:47:53 +0900

    Create Roat.cs

---
+++ Roat.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.Rotate(0, 0, 45);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
